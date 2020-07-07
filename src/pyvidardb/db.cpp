634c19b3c4d1556caefbbeae85f72153fb993a3f
xingweitian <xingweitian@gmail.com>
2020-07-07 01:32:31 -0400

    Add some document for DB.

--- src/pyvidardb/db.cpp
+++ src/pyvidardb/db.cpp
@@ -7,9 +7,13 @@ namespace py = pybind11;
 void init_db(py::module &m) {
   py::class_<py_DB>(m, "DB")
       .def(py::init<>())
-      .def("open", &py_DB::Open)
-      .def("put", &py_DB::Put)
-      .def("get", &py_DB::Get)
-      .def("delete", &py_DB::Delete)
-      .def("close", &py_DB::Close);
+      .def("open", &py_DB::Open,
+           "Open the database, will create one if not exist.")
+      .def("put", &py_DB::Put,
+           "Put a map from 'key' to 'value' into the database.")
+      .def("get", &py_DB::Get,
+           "Get the value of the provided key, will return None if there is no "
+           "such key in the database.")
+      .def("delete", &py_DB::Delete, "Remove the 'key' from the database.")
+      .def("close", &py_DB::Close, "Close the database.");
 }
