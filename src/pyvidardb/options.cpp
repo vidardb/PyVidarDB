ac85450f5e5cfce50b1bd6ef7889c0e2a38524b5
Weitian Xing <xingweitian@gmail.com>
2020-07-17 20:50:27 -0400

    Improve the exposed options' names.

--- src/pyvidardb/options.cpp
+++ src/pyvidardb/options.cpp
@@ -8,13 +8,12 @@ namespace py = pybind11;
 void init_option(py::module &m) {
   py::class_<DBOptions, std::unique_ptr<DBOptions>>(m, "DBOptions")
       .def(py::init<>())
-      .def_readwrite("max_background_compactions",
+      .def_readwrite("max_background_parallelism",
                      &DBOptions::max_background_compactions);
 
   py::class_<ColumnFamilyOptions>(m, "ColumnFamilyOptions")
       .def(py::init<>())
-      .def_readwrite("write_buffer_size",
-                     &ColumnFamilyOptions::write_buffer_size);
+      .def_readwrite("ram_size", &ColumnFamilyOptions::write_buffer_size);
 
   py::class_<Options, DBOptions, ColumnFamilyOptions>(m, "Options")
       .def(py::init<>());
