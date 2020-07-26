276319f77eb17f6b5a171da031847682f80ef339
Weitian Xing <xingweitian@gmail.com>
2020-07-26 18:21:55 -0400

    Test iterator in simple_example.py.

--- examples/simple_example.py
+++ examples/simple_example.py
@@ -14,6 +14,14 @@ assert key_not_exist is None
 
 db.compact()
 
+db_iter = db.iter()
+
+while db_iter.valid():
+    key, value = db_iter.key(), db_iter.value()
+    assert key == b"key1"
+    assert value == b"value1"
+    db_iter.next()
+
 db.delete(b"key1")
 
 db.close()
