42fc69f9b6a760f6129eb5a07eaf0e02099ce566
Weitian Xing <xingweitian@gmail.com>
2020-07-30 01:56:47 -0400

    Ready to upload to PyPI. (#6)

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, Extension, find_packages
 from setuptools.command.build_ext import build_ext
 
 if sys.version_info < (3, 0):
-    sys.exit('Sorry, Python < 3 is not supported by PyVidarDB.')
+    sys.exit("Sorry, Python < 3 is not supported by PyVidarDB.")
 
 
 class CMakeExtension(Extension):
@@ -26,43 +26,62 @@ class CMakeBuild(build_ext):
             out = subprocess.check_output(["cmake", "--version"])
         except OSError:
             raise RuntimeError(
-                "CMake must be installed to build the following extensions: " +
-                ", ".join(e.name for e in self.extensions))
+                "CMake must be installed to build the following extensions: "
+                + ", ".join(e.name for e in self.extensions)
+            )
 
         if platform.system() == "Windows":
-            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
-            if cmake_version < '3.1.0':
+            cmake_version = LooseVersion(
+                re.search(r"version\s*([\d.]+)", out.decode()).group(1)
+            )
+            if cmake_version < "3.1.0":
                 raise RuntimeError("CMake >= 3.1.0 is required on Windows")
 
         for ext in self.extensions:
             self.build_extension(ext)
 
     def build_extension(self, ext):
-        ext_dir = os.path.abspath(
-            os.path.dirname(self.get_ext_fullpath(ext.name)))
+        ext_dir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
 
-        cmake_args = ["-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + ext_dir,
-                      "-DPYTHON_EXECUTABLE=" + sys.executable]
+        cmake_args = [
+            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + ext_dir,
+            "-DPYTHON_EXECUTABLE=" + sys.executable,
+        ]
 
         cfg = "Debug" if self.debug else "Release"
         build_args = ["--config", cfg]
 
+        # Pile all .so in one place and use $ORIGIN as RPATH
+        cmake_args += ["-DCMAKE_BUILD_WITH_INSTALL_RPATH=TRUE"]
+        cmake_args += ["-DCMAKE_INSTALL_RPATH={}".format("$ORIGIN")]
+
         if platform.system() == "Windows":
-            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), ext_dir)]
+            cmake_args += [
+                "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), ext_dir)
+            ]
             if sys.maxsize > 2 ** 32:
-                cmake_args += ['-A', 'x64']
-            build_args += ['--', '/m']
+                cmake_args += ["-A", "x64"]
+            build_args += ["--", "/m"]
         else:
-            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-            build_args += ['--', '-j4']
+            cmake_args += ["-DCMAKE_BUILD_TYPE=" + cfg]
+            build_args += ["--", "-j4"]
+
+        env = os.environ.copy()
+        env["CXXFLAGS"] = '{} -DVERSION_INFO=\\"{}\\"'.format(
+            env.get("CXXFLAGS", ""), self.distribution.get_version()
+        )
 
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
 
-        subprocess.check_call(["cmake", ext.source_dir] + cmake_args,
-                              cwd=self.build_temp)
-        subprocess.check_call(["cmake", "--build", "."] + build_args,
-                              cwd=self.build_temp)
+        subprocess.check_call(
+            ["cmake", ext.source_dir] + cmake_args, cwd=self.build_temp, env=env
+        )
+        subprocess.check_call(
+            ["cmake", "--build", ".", "--target", os.path.basename(ext.name)]
+            + build_args,
+            cwd=self.build_temp,
+        )
 
 
 setup(
@@ -71,8 +90,15 @@ setup(
     author="VidarDB Team",
     author_email="info@vidardb.com",
     description="Python Wrapper for VidarDB",
+    long_description_content_type="text/markdown",
     long_description=open("README.md", encoding="utf-8").read(),
     url="https://github.com/vidardb/PyVidarDB",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.5",
     packages=find_packages("src"),
     package_dir={"": "src"},
     ext_modules=[CMakeExtension("pyvidardb/pyvidardb")],
