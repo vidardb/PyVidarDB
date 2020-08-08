2b789ec7c5e21955767426b270ec783fafcb6721
Weitian Xing <xingweitian@gmail.com>
2020-08-07 23:03:28 -0400

    Add more experiments.

--- experiment/lmdb_experiment.py
+++ experiment/lmdb_experiment.py
@@ -2,6 +2,7 @@ import os
 import random
 import shutil
 import time
+from datetime import datetime
 
 import lmdb
 
@@ -10,26 +11,53 @@ import lmdb
 
 
 db_name = "lmdb_experiment.lmdb"
+put_range = 1000000  # 200000000
+latest_data_num = 5000
+random_get_range = 5000
+
+data_list = list()
+latest_data_list = list()
 
 if os.path.exists(db_name):
     shutil.rmtree(db_name)
 
 env = lmdb.open(db_name, map_size=80 * 1024 * 1024 * 1024)
-# latest_data = []
+
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+
+print("-------------------------------------")
+print("Experiment start at: ", current_time)
+print("[Put Data Number]: ", put_range)
+print("[Latest Data Number]:", latest_data_num)
+print("[Random Get Number]:", random_get_range)
 
 put_start = time.time()
 with env.begin(write=True) as txn:
-    for each in range(200000000):
+    for each in range(put_range):
         k = str(random.uniform(1, 10000)).encode()
         txn.put(k, k)
-        # if each > 990000:
-        #     latest_data.append(k)
+        data_list.append(k)
+        if each > (put_range - latest_data_num):
+            latest_data_list.append(k)
 put_end = time.time()
 print("[Put]:", str(put_end - put_start))
 
-# get_start = time.time()
-# with env.begin() as txn:
-#     for each in latest_data:
-#         txn.get(each)
-# get_end = time.time()
-# print("[Get]:", str(get_end - get_start))
+get_start = time.time()
+with env.begin() as txn:
+    for each in latest_data_list:
+        txn.get(each)
+get_end = time.time()
+print("[Get latest data]:", str(get_end - get_start))
+
+get_start = time.time()
+with env.begin(write=True) as txn:
+    for each in range(random_get_range):
+        k = data_list[random.randint(0, len(data_list) - 1)]
+        txn.get(k)
+        txn.delete(k)
+get_end = time.time()
+print("[Get random data & delete]:", str(get_end - get_start))
+
+print("-------------------------------------")
+print()
--- experiment/vidardb_experiment.py
+++ experiment/vidardb_experiment.py
@@ -2,6 +2,7 @@ import os
 import random
 import shutil
 import time
+from datetime import datetime
 
 import pyvidardb
 
@@ -9,7 +10,12 @@ import pyvidardb
 #     return ''.join(random.choice(string.digits) for _ in range(n))
 
 db_name = "vidardb_experiment.vidardb"
-# latest_data = []
+put_range = 1000000  # 200000000
+latest_data_num =  5000
+random_get_range = 5000
+
+data_list = list()
+latest_data_list = list()
 
 if os.path.exists(db_name):
     shutil.rmtree(db_name)
@@ -17,19 +23,43 @@ if os.path.exists(db_name):
 db = pyvidardb.DB()
 db.open(db_name)
 
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+
+print("-------------------------------------")
+print("Experiment start at: ", current_time)
+print("[Put Data Number]: ", put_range)
+print("[Latest Data Number]:", latest_data_num)
+print("[Random Get Number]:", random_get_range)
+
 put_start = time.time()
-for each in range(200000000):
+for each in range(put_range):
     # k = random_string_generator(10).encode()
-    # v = random_string_generator(10).encode()
     k = str(random.uniform(1, 10000)).encode()
     db.put(k, k)
+    data_list.append(k)
+    if each > (put_range - latest_data_num):
+        latest_data_list.append(k)
 put_end = time.time()
 print("[Put]:", str(put_end - put_start))
 
-# get_start = time.time()
-# for each in latest_data:
-#     db.get(each)
-# get_end = time.time()
-# print("[Get]:", str(get_end - get_start))
+get_start = time.time()
+for each in latest_data_list:
+    db.get(each)
+get_end = time.time()
+print("[Get latest data]:", str(get_end - get_start))
+
+db.compact()
+
+get_start = time.time()
+for each in range(random_get_range):
+    k = data_list[random.randint(0, len(data_list) - 1)]
+    db.get(k)
+    db.delete(k)
+get_end = time.time()
+print("[Get random data & delete]:", str(get_end - get_start))
+
+print("-------------------------------------")
+print()
 
 db.close()
